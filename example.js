edc3f30014daf178afb20b10411bed2e99e0a44f
Julian Gruber <julian@juliangruber.com>
2020-02-15 12:34:06 +0100

    example

--- example.js
+++ example.js
@@ -1,9 +1,13 @@
 const dependants = require('.')
 
 const main = async () => {
-  for await (const dependant of dependants('express')) {
+  const name = process.argv[2] || 'express'
+  for await (const dependant of dependants(name)) {
     console.log(dependant)
   }
 }
 
-main()
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
