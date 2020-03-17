82e1d6bc62782a08d1489b9095ee8d35fb5812b5
Julian Gruber <julian@juliangruber.com>
2020-03-17 21:55:19 +0100

    ops: add limit option

--- bin/ops.js
+++ bin/ops.js
@@ -4,17 +4,24 @@ const dependants = require('..')
 const { ux } = require('@cto.ai/sdk')
 
 const main = async () => {
-  const { name } = await ux.prompt([
+  const { name, limit } = await ux.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'Name of the module to search',
       default: 'express'
+    }, {
+      type: 'input',
+      name: 'limit',
+      message: 'How many modules to display at most',
+      default: '10'
     }
   ])
 
+  let displayed = 0
   for await (const dependant of dependants(name)) {
     await ux.print(dependant)
+    if (++displayed === Number(limit)) break
   }
 }
 
